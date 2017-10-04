db4e02c0394e2d8fa9d80d5fa25aa19649e2345a
Raku Zeta <zetaraku@gmail.com>
2017-10-05 00:26:50 +0800

    add a version of traveling_salesman that use string instead of integer as key

--- dynamic_programming/traveling_salesman.js
+++ dynamic_programming/traveling_salesman.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const { createNDimArray } = require('../util/ndim_arr');
 
-function traveling_salesman(adj_matrix) {
+function traveling_salesman_1(adj_matrix) {
 	let n = adj_matrix.length;
 
 	/*
@@ -97,6 +97,97 @@ function traveling_salesman(adj_matrix) {
 	}
 }
 
+/*
+	Use string instead of integer as key so that the size can be arbitrarily large.
+*/
+function traveling_salesman_2(adj_matrix) {
+	let n = adj_matrix.length;
+
+	/*
+		dp_distance_to_0[i][S] =
+			shortest distance from node i to node 0 using all nodes in S as intermediate nodes once
+		S is a set in STRING bitmask representation which k-th CHAR ('0'/'1') is corresponding to k-th node
+	*/
+	let dp_distance_to_0 = createNDimArray([n]);
+
+	/*
+		dp_nextnode_to_0[i][S] =
+			the next node of the shortest path if we're at i and want to go to 0
+			using all nodes in S as intermediate nodes once
+	*/
+	let dp_nextnode_to_0 = createNDimArray([n]);
+
+	const EMPTY_SET = '0'.repeat(n);
+	const ALL_NODES = '1'.repeat(n);
+
+	for(let i = 0; i < n; i++) {
+		dp_distance_to_0[i] = {};	// initialize 2nd dimension here
+		dp_distance_to_0[i][EMPTY_SET] = adj_matrix[i][0];
+		dp_nextnode_to_0[i] = {};	// initialize 2nd dimension here
+	}
+
+	for(let setSize = 1; setSize <= n-1; setSize++) {
+		for(let A of chooseFromSet(bitwise_minus(ALL_NODES, 0), setSize)) {
+			for(let i of enumerate_bitset(bitwise_not(A))) {
+				dp_distance_to_0[i][A] = +Infinity;
+				for(let j of enumerate_bitset(A)) {
+					let distance = adj_matrix[i][j] + dp_distance_to_0[j][bitwise_minus(A, j)];
+					if(distance < dp_distance_to_0[i][A]) {
+						dp_distance_to_0[i][A] = distance;
+						dp_nextnode_to_0[i][A] = j;
+					}
+				}
+			}
+		}
+	}
+
+	return {
+		min_distance: dp_distance_to_0[0][bitwise_minus(ALL_NODES, 0)],
+		shortest_path: reconstructPath(dp_nextnode_to_0, ALL_NODES),
+		distance_matrix: dp_distance_to_0,
+		nextnode_matrix: dp_nextnode_to_0,
+	};
+
+	function reconstructPath(nextnode_matrix, all_nodes) {
+		let i = 0, S = bitwise_minus(all_nodes, 0);
+
+		let _path = [0];
+		while(S.includes('1')) {
+			i = nextnode_matrix[i][S];
+			S = bitwise_minus(S, i);
+			_path.push(i);
+		}
+
+		return _path;
+	}
+
+	function* chooseFromSet(S, k) {
+		if(k === 0) {
+			yield '0'.repeat(S.length);
+		} else if(!S.includes('1')) {
+			return;
+		} else {
+			for(let value of chooseFromSet(S.slice(1), k))
+				yield '0' + value;
+			if(S.charAt(0) === '1') {
+				for(let value of chooseFromSet(S.slice(1), k-1))
+					yield '1' + value;
+			}
+		}
+	}
+	function* enumerate_bitset(S) {
+		for(let k = 0; k < S.length; k++) {
+			if(S.charAt(k) === '1')
+				yield k;
+		}
+	}
+	function bitwise_minus(S, nth) {
+		return S.slice(0, nth) + '0' + S.slice(nth + 1);
+	}
+	function bitwise_not(S) {
+		return S.replace(/[01]/g, (m0) => m0 === '0' ? '1' : '0');
+	}
+}
 
 function test() {
 	let adj_matrix = [
@@ -113,13 +204,27 @@ function test() {
 		shortest_path: [0, 2, 3, 1],
 	};
 
-	let result = traveling_salesman(adj_matrix);
+	// test traveling_salesman_1
+	{
+		let result = traveling_salesman_1(adj_matrix);
 
-	console.log('min distance:', result.min_distance);
-	console.log('path:', result.shortest_path);
+		console.log('min distance:', result.min_distance);
+		console.log('path:', result.shortest_path);
 
-	assert.equal(result.min_distance, expected_result.min_distance);
-	assert.deepStrictEqual(result.shortest_path, expected_result.shortest_path);
+		assert.equal(result.min_distance, expected_result.min_distance);
+		assert.deepStrictEqual(result.shortest_path, expected_result.shortest_path);
+	}
+
+	// test traveling_salesman_2
+	{
+		let result = traveling_salesman_2(adj_matrix);
+
+		console.log('min distance:', result.min_distance);
+		console.log('path:', result.shortest_path);
+
+		assert.equal(result.min_distance, expected_result.min_distance);
+		assert.deepStrictEqual(result.shortest_path, expected_result.shortest_path);
+	}
 }
 
 test();
