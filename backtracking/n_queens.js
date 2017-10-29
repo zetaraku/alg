a495237b96b703245988ab679d02654d33af759e
Raku Zeta <zetaraku@gmail.com>
2017-10-29 23:23:54 +0800

    extract 1 util

--- backtracking/n_queens.js
+++ backtracking/n_queens.js
@@ -1,33 +1,5 @@
 const assert = require('assert');
-
-/*
-	A special implement of linked list
-	which can temporarily remove the iterating item when iterating
-*/
-class Node {
-	constructor(data) {
-		this.data = data;
-		this.next = null;
-	}
-}
-class SkippingLinkedList {
-	constructor(datas) {
-		this.primer = new Node();
-		let cur = this.primer;
-		for(let data of datas) {
-			cur.next = new Node(data);
-			cur = cur.next;
-		}
-	}
-	* [Symbol.iterator]() {
-		for(let n = this.primer; n.next !== null; n = n.next) {
-			let t = n.next;
-			n.next = t.next;	// temporarily remove the returning node
-			yield t.data;
-			n.next = t;			// put it back afterward
-		}
-	}
-}
+const SkippingLinkedList = require('../util/SkippingLinkedList');
 
 // algorithm to find the solutions of the 'eight queens puzzle'
 function n_queens(n) {
