234f529610261632ae9597270e91691389394f5f
Raku Zeta <zetaraku@gmail.com>
2017-11-05 21:20:24 +0800

    init test

---
+++ test/test_00_template.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+// const chai = require('chai');
+// const expect = chai.expect;
+// let should = chai.should();
+// let assert = chai.assert;
+
+describe('chap.00 template', function() {
+	describe('<alg_name>', function() {
+		it('correct', () => {}); return;
+
+		let { /* required functions */ } = require('../<catalog>/<alg_name>');
+
+		// insert preparation
+
+		it('correct', function() {
+			// insert test check
+		});
+	});
+});
