3e819f00117ba0add37f13c97ae107b9d37f5d15
Raku Zeta <zetaraku@gmail.com>
2017-11-15 10:11:25 +0800

    move 2 sorting alg from divide_and_conquer/, add 1 alg

--- sorting/merge_sort.js
+++ sorting/merge_sort.js
@@ -1,6 +1,53 @@
 const defaultCompareFunction = (a, b) => (a < b ? -1 : a > b ? +1 : 0);
 
 // stable
+function merge_sort(data, compare = defaultCompareFunction) {
+	let n = data.length;
+
+	let buffer = Array(n);
+
+	msort(0, n);
+
+	function msort(begin, end) {
+		if(end - begin < 2)
+			return;
+		let mid = Math.floor((begin + end) / 2);
+		msort(begin, mid);
+		msort(mid, end);
+		merge(begin, mid, end);
+	}
+
+	function merge(begin, mid, end) {
+		// data[i] or data[j] will be invalid if any interval is empty
+		if(begin >= mid || mid >= end)
+			return;		// if so, simply do nothing
+
+		let i = begin, j = mid;
+		let k = begin;
+		while(true) {
+			if(compare(data[i], data[j]) <= 0) {
+				buffer[k++] = data[i++];
+				if(i >= mid) {
+					while(j < end)
+						buffer[k++] = data[j++];
+					break;
+				}
+			} else {
+				buffer[k++] = data[j++];
+				if(j >= end) {
+					while(i < mid)
+						buffer[k++] = data[i++];
+					break;
+				}
+			}
+		}
+
+		for(let t = begin; t < end; t++)
+			data[t] = buffer[t];
+	}
+}
+
+// stable
 function merge_sort_iterative(data, compare = defaultCompareFunction) {
 	let n = data.length;
 	let lgm = Math.ceil(Math.log2(n));
@@ -120,6 +167,7 @@ function merge_sort_modifylink(data, compare = defaultCompareFunction) {
 }
 
 module.exports = {
+	merge_sort,
 	merge_sort_iterative,
 	merge_sort_modifylink,
 };
--- sorting/quick_sort.js
+++ sorting/quick_sort.js
@@ -1,6 +1,88 @@
 const defaultCompareFunction = (a, b) => (a < b ? -1 : a > b ? +1 : 0);
 
 // unstable
+function quick_sort(data, compare = defaultCompareFunction) {
+	let n = data.length;
+
+	qsort(0, n-1);
+
+	function qsort(from, to) {
+		if(to - from + 1 < 2)
+			return;
+		let pivot_index = partition(from, to);
+		qsort(from, pivot_index - 1);
+		qsort(pivot_index + 1, to);
+	}
+
+	function partition(from, to) {	// [from, to] should be non-empty
+		let pivot_index = selectPivotIndex(from, to);
+		let pivot = data[pivot_index];
+
+		let j = from - 1 /* right wall */;
+
+		[data[from], data[pivot_index]] = [data[pivot_index], data[from]];
+		for(let i = from; i <= to; i++) {
+			if(compare(data[i], pivot) <= 0) {
+				j++;
+				[data[i], data[j]] = [data[j], data[i]];
+			}
+		}
+		[data[from], data[j]] = [data[j], data[from]];
+
+		return j;
+	}
+
+	function selectPivotIndex(from, to) {
+		return from + Math.floor((to - from + 1) * Math.random());
+	}
+}
+
+// stable version
+function quick_sort_stable(data, compare = defaultCompareFunction) {
+	let n = data.length;
+
+	qsort(0, n);
+
+	function qsort(begin, end) {
+		if(end - begin < 2)
+			return;
+		let [pivot_begin, pivot_end] = partition(begin, end);
+		qsort(begin, pivot_begin);
+		qsort(pivot_end, end);
+	}
+
+	function partition(begin, end) {
+		let pivot = data[selectPivotIndex(begin, end)];
+
+		let less = [], equal = [], greater = [];
+		for(let i = begin; i < end; i++) {
+			let compare_result = compare(data[i], pivot);
+			if(compare_result < 0) {
+				less.push(data[i]);
+			} else if(compare_result > 0) {
+				greater.push(data[i]);
+			} else {
+				equal.push(data[i]);
+			}
+		}
+
+		let k = begin;
+		for(let value of less)
+			data[k++] = value;
+		for(let value of equal)
+			data[k++] = value;
+		for(let value of greater)
+			data[k++] = value;
+
+		return [begin + less.length, end - greater.length];
+	}
+
+	function selectPivotIndex(begin, end) {
+		return begin + Math.floor((end - begin) * Math.random());
+	}
+}
+
+// unstable
 function quick_sort_bidirectional(data, compare = defaultCompareFunction) {
 	let n = data.length;
 
@@ -38,4 +120,8 @@ function quick_sort_bidirectional(data, compare = defaultCompareFunction) {
 	}
 }
 
-module.exports = { quick_sort_bidirectional };
+module.exports = {
+	quick_sort,
+	quick_sort_stable,
+	quick_sort_bidirectional,
+};
